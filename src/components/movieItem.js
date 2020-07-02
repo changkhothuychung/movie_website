e32eae59ed702fc4ff9e44dd27378fc5f40ddd36
NhatNguyen1810 <nhat7203@gmail.com>
2020-07-01 19:11:26 -0700

    update

--- src/components/movieItem.js
+++ src/components/movieItem.js
@@ -6,11 +6,14 @@ import {useParams} from 'react-router-dom';
 import {getMovieById,getAllMovie} from '../schema/schema';
 import { Spin } from 'antd';
 import './movieItem.css';
+import './movieItemCondition.css';
 import axios from 'axios';
 import {Link} from 'react-router-dom'
 import {LeftOutlined, ClockCircleFilled,StarFilled} from '@ant-design/icons';
 
 const { Meta } = Card;
+
+
 
 
 
@@ -41,7 +44,11 @@ const MovieItem = (props) => {
         },
     });
     const [backgroundImg, backgroundImgState] = useState();
+    const [isDesktop, setDesktop] = useState(window.innerWidth);
 
+    const updateMedia = () => {
+        setDesktop(window.innerWidth);
+    };
     
 
     const {data, loading, error} = useQuery(getMovieById, {
@@ -56,34 +63,140 @@ const MovieItem = (props) => {
     }
 
     const {data2, loading2, error2} = useQuery(getAllMovie);
+    
 
     useEffect( () => {
        
-
-        const data = async () => {
-            await axios.get(`https://api.themoviedb.org/3/movie/${id}?api_key=0aecc06bb4fadb06b5f071fef0c2ce6d&&language=en-US&append_to_response=videos%2Cimages&include_image_language=en%2Cnull&fbclid=IwAR0YGcvMYcFf8ieH8yGGDQZuxZ159CdSyQ_YXMqvPgamKd0jPSjWlZELaEg`
-            ).then(result => {
+        window.addEventListener("resize", updateMedia);
+        let data = null;
+        console.log(isDesktop)
+        if(isDesktop > 800){
+            console.log("first condition")
+             data = async () => {
+                await axios.get(`https://api.themoviedb.org/3/movie/${id}?api_key=0aecc06bb4fadb06b5f071fef0c2ce6d&&language=en-US&append_to_response=videos%2Cimages&include_image_language=en%2Cnull&fbclid=IwAR0YGcvMYcFf8ieH8yGGDQZuxZ159CdSyQ_YXMqvPgamKd0jPSjWlZELaEg`
+                ).then(result => {
+                    
+                    console.log("hello world")     
                 
-                console.log("hello world")     
-            
-                senditemState({movies: result.data});
-                console.log(senditem);
-                backgroundImgState({
-                    background: `linear-gradient(rgba(0, 0, 0, 0.97) 10%,
-                                    rgba(0, 0, 0, 0.92) 20%, 
-                                    rgba(0, 0, 0, 0.92) 80%, 
-                                    rgba(0, 0, 0, 0.97) 100%) 0% 0% / cover, 
-                                    url(https://image.tmdb.org/t/p/w300${result.data.backdrop_path}),
-                                    center center no-repeat rgb(44, 57, 73)`
-                                    
+                    senditemState({movies: result.data});
+                    console.log(senditem);
+                    backgroundImgState({
+                        background: `linear-gradient(rgba(0, 0, 0, 0.97) 10%,
+                                        rgba(0, 0, 0, 0.92) 20%, 
+                                        rgba(0, 0, 0, 0.92) 80%, 
+                                        rgba(0, 0, 0, 0.97) 100%) 0% 0% / cover, 
+                                        url(https://image.tmdb.org/t/p/w300${result.data.backdrop_path}),
+                                        center center no-repeat rgb(44, 57, 73)`
+                                        
+                    })
+                }).catch(err => {
+                    console.log(err);
                 })
-            }).catch(err => {
-                console.log(err);
-            })
+            }
+        
+        }
+
+        else{
+             data = async () => {
+                console.log("second condition")
+                await axios.get(`https://api.themoviedb.org/3/movie/${id}?api_key=0aecc06bb4fadb06b5f071fef0c2ce6d&&language=en-US&append_to_response=videos%2Cimages&include_image_language=en%2Cnull&fbclid=IwAR0YGcvMYcFf8ieH8yGGDQZuxZ159CdSyQ_YXMqvPgamKd0jPSjWlZELaEg`
+                ).then(result => {
+                    
+                    console.log("hello world")     
+                
+                    senditemState({movies: result.data});
+                    console.log(senditem);
+                    backgroundImgState({
+                        background: `linear-gradient(rgba(0, 0, 0, 0), 
+                        rgba(0, 0, 0, 0) 50%, 
+                        rgba(44, 57, 73, 0.8) 60%, 
+                        rgb(44, 57, 73) 70%, 
+                        rgba(44, 57, 73, 0.99) 100%) 0% 0% / cover, 
+                        url(https://image.tmdb.org/t/p/w780${result.data.backdrop_path})
+                        center
+                        center
+                        no-repeat`,
+                        backgroundSize: '100% 100%',
+                                        
+                    })
+                }).catch(err => {
+                    console.log(err);
+                })
+            }
         }
         data();
         
-    }, []);
+    }, [isDesktop]);
+
+
+    const MovieItemConditionally = () => {
+        return(
+            <React.Fragment>
+                <div style={backgroundImg} className="container-condition">
+                    <div>
+                        <Link to="/popularmovie">   
+                            <button className="button-condition">Back</button>
+                        </Link>
+                    </div>
+                    <div className="item-container-condition">
+                        <h1 className="item-title-condition">{senditem.movies.title}</h1>
+                        <h3 className="item-tagline-condition">{senditem.movies.tagline}</h3>
+                        <div className="item-genres-condition">
+                            {senditem.movies.genres.map((item, index) => {
+                                return(
+                                    <div className="item-genres-generator">
+                                        {index == senditem.movies.genres.length - 1 ?
+                                            (
+                                                <p>{item.name}</p>
+                                            )
+                                        
+                                            :
+                                            (
+                                                <p>{item.name} </p>
+                                            )
+                                        }
+                                    </div>
+                                )
+                            })}
+                        </div>
+                        <div className="item-time-star-condition">
+                            <div className="time-condition">
+                                <p><ClockCircleFilled /></p>
+                                <p>{convertToHours(senditem.movies.runtime)}</p>
+                            </div>
+                            <div className="star-condition">
+                                <p><StarFilled /></p>
+                                <p>{senditem.movies.vote_average}</p>
+                            </div>
+                        </div>
+                        <div className="item-footer-condition">
+                            <p className="item-overview-condition">
+                                 {senditem.movies.overview}
+                            </p>
+                            <div className="revenue-budget-condition">
+                                 <div>
+                                    
+                                    <p>Revenue: ${senditem.movies.revenue.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</p>
+                                </div>
+        
+                                <div>
+                                    
+                                    <p>Budget: ${senditem.movies.budget.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</p>
+                                </div>
+            
+                            </div>
+                        </div>
+
+                    </div>
+                    <div className="button-option-condition">
+                            <button className="button1-condition" ><a href={`https://www.youtube.com/watch?v=${senditem.movies.videos.results[0].key}`} target="_blank" >Watch Trailer</a></button>
+                            <button className="button2-condition" onClick={() => props.addMovie(data.searchById[0])} >Add To List</button>
+                    </div>
+                </div>
+            </React.Fragment>
+        )
+    }
+    
 
 
     if(loading){
@@ -106,17 +219,15 @@ const MovieItem = (props) => {
     }
 
 
-   
-
-
-
-
     return(
         <React.Fragment  >
-                {!senditem.movies  ? (
+            {}
+                {!senditem.movies  ?  (
                     <h1>hihih</h1>
-                ) : (
-                    <div style={backgroundImg} className="container">
+                ) : ( 
+                <div>
+                    {isDesktop > 800 ? (
+                        <div style={backgroundImg} className="container">
 
                     
                         <Link to="/popularmovie">
@@ -125,117 +236,125 @@ const MovieItem = (props) => {
                         </Link>
                    
 
-                    <div style={backgroundImg} className="item-container">
+                         <div  className="item-container">
 
-                    <div className="item-left">
-                        
-                        <div className="item-left-movie" >
-                            <div className="item-left-img" >
-                                <img 
-                                    alt="example" 
-                                    src={`https://image.tmdb.org/t/p/w300${data.searchById[0].poster_path}`} />
+                            <div className="item-left">
+
+                               
+                                <div className="item-left-movie" >
+                                    <div className="item-left-img" >
+                                        <img 
+                                            alt="example" 
+                                            src={`https://image.tmdb.org/t/p/w300${data.searchById[0].poster_path}`} />
+                                    </div>
+                                </div>
+                                
+                                
+                                
+            
+                                <div className="item-left-overview">
+                                    <p>{senditem.tagline}</p>
+                                </div>
+            
+                                <div className="item-left-button">
+                                
+                                    <button className="button1" ><a href={`https://www.youtube.com/watch?v=${senditem.movies.videos.results[0].key}`} target="_blank" >Watch Trailer</a></button>
+                                    <button className="button2" onClick={() => props.addMovie(data.searchById[0])} >Add To List</button>
+            
+                                </div>
+            
                             </div>
-                        </div>
-    
-                        <div className="item-left-overview">
-                            <p>{senditem.tagline}</p>
-                        </div>
-    
-                        <div className="item-left-button">
-                        
-                            <button className="button1" ><a href={`https://www.youtube.com/watch?v=${senditem.movies.videos.results[0].key}`} target="_blank" >Watch Trailer</a></button>
-                            <button className="button2" onClick={() => props.addMovie(data.searchById[0])} >Add To List</button>
-    
-                        </div>
-    
-                    </div>
+                            
+                            {/* the first component */}
     
     
-                    <div className="item-middle">
-                        
-    
-                        <div className="item-middle-genres">
-                            <h1 className="item-middle-genres-title">{senditem.movies.title}</h1>
-                            <div className="item-middle-genres-list">
-                            {senditem.movies.genres.map((item, index) => {
-                                if(index == senditem.movies.genres.length - 1){
-                                    return(
-                                         <p>{item.name}</p>
+                            <div className="item-middle">
+                                
+            
+                                <div className="item-middle-genres">
+                                    <h1 className="item-middle-genres-title">{senditem.movies.title}</h1>
+                                    <div className="item-middle-genres-list">
+                                    {senditem.movies.genres.map((item, index) => {
+                                        if(index == senditem.movies.genres.length - 1){
+                                            return(
+                                                <p>{item.name}</p>
+                                            )
+                                        }
+                                        else{
+                                            return(
+                                                <p>{item.name + ", " + " "} <span> </span></p>
+                                        )
+                                        }
+                                    })}
+                                    </div>
+
+                                    <div className="item-middle-genres-time-vote">
+                                            <div className="time">
+                                                <p><ClockCircleFilled /></p>
+                                                <p>{convertToHours(senditem.movies.runtime)}</p>
+                                            </div>
+                                            <div className="star">
+                                                <p><StarFilled /></p>
+                                                <p>{senditem.movies.vote_average}</p>
+                                            </div>
+                                    </div>
+                                </div>
+            
+                                <div className="item-middle-time-start">
+            
+                                </div>
+            
+                                <div className="item-middle-overview">
+                                    <h2>Overview</h2>
+                                    <p>{senditem.movies.overview}</p>
+                                </div>
+            
+                                <div className="item-middle-addinfo">
+                                    <div>
+                                        <h3>Revenue</h3>
+                                        <p>{senditem.movies.revenue.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</p>
+                                    </div>
+            
+                                    <div>
+                                        <h3>Budget</h3>
+                                        <p>{senditem.movies.budget.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</p>
+                                    </div>
+            
+                                    <div>
+                                        <h3>Release Date</h3>
+                                        <p>{senditem.movies.release_date}</p>
+                                    </div>
+                                </div>
+                                
+                            </div>
+
+                           
+                            <div className="item-right">
+        
+                                {
+                                    senditem.movies.images.backdrops.map((item,index) => {
+                                        if(index < 3){
+                                            return(
+                                                <div className="item-right-img">
+                                                    <img  src={`https://image.tmdb.org/t/p/w300${item.file_path}`} />
+                                                </div>
+                                                )
+                                            }
+                                        }
                                     )
                                 }
-                                else{
-                                    return(
-                                        <p>{item.name + ", " + " "} <span> </span></p>
-                                   )
-                                }
-                            })}
                             </div>
-
-                            <div className="item-middle-genres-time-vote">
-                                    <div className="time">
-                                        <p><ClockCircleFilled /></p>
-                                        <p>{convertToHours(senditem.movies.runtime)}</p>
-                                    </div>
-                                    <div className="star">
-                                        <p><StarFilled /></p>
-                                        <p>{senditem.movies.vote_average}</p>
-                                    </div>
-                            </div>
-                        </div>
-    
-                        <div className="item-middle-time-start">
-    
-                        </div>
-    
-                        <div className="item-middle-overview">
-                            <h2>Overview</h2>
-                            <p>{senditem.movies.overview}</p>
-                        </div>
-    
-                        <div className="item-middle-addinfo">
-                            <div>
-                                <h3>Revenue</h3>
-                                <p>{senditem.movies.revenue.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</p>
-                            </div>
-    
-                            <div>
-                                <h3>Budget</h3>
-                                <p>{senditem.movies.budget.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</p>
-                            </div>
-    
-                            <div>
-                                <h3>Release Date</h3>
-                                <p>{senditem.movies.release_date}</p>
-                            </div>
-                        </div>
-                        
-                    </div>
-    
-                    <div className="item-right">
-    
-    
-                        {
-    
-                            senditem.movies.images.backdrops.map((item,index) => {
-    
-                                if(index < 3){
-                                    return(
-                                        <div className="item-right-img">
-                                            <img  src={`https://image.tmdb.org/t/p/w300${item.file_path}`} />
-                                        </div>
-                                        )
-                                    }
-                                }
-                            )
-                        
-                        }
-                        
                             
-                    </div>
+            
+                    
     
+                        </div>
                     </div>
-                    </div>
+                    ) : (
+                        <MovieItemConditionally />
+                    )}
                 
+                </div>
                 )}
            
                 
